b31d5e72bc394e98fc9c94a685037f553c31fb43
Marcos Maverick <marquinhopimpo@gmail.com>
2022-03-15 15:08:22 -0300

    README.md folder changes

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
+import morgan from 'morgan';
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import connectDB from './db/connect.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
 
+
+if(process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 // app.use(cors());
 app.use(express.json());
 
