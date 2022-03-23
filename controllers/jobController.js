c247b9d46713e02776bf87a5f03f38c4e6e871e7
Marcos Maverick <marquinhopimpo@gmail.com>
2022-03-23 18:06:03 -0300

    create a function to check permission before edit and delete job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,57 +1,70 @@
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 import Job from "../models/Job.js";
+import checkPermission from "../utils/checkPermission.js";
 
 const createJob = async (req, res) => {
-    const { position, company } = req.body;
+  const { position, company } = req.body;
 
-    if(!position || !company) {
-        throw new BadRequestError('Please provide all values')
-    }
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
 
-    req.body.createdBy = req.user.userId;
-    const job = await Job.create(req.body)
-    
-    res.status(StatusCodes.CREATED).json({job})
-}
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({createdBy: req.user.userId})
+  const jobs = await Job.find({ createdBy: req.user.userId });
 
-    res.status(StatusCodes.OK).json({
-        jobs,
-        totalJobs: jobs.length,
-        numOfPages: 1
-    })
-}
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs: jobs.length,
+    numOfPages: 1,
+  });
+};
 const updateJob = async (req, res) => {
-    const {id: jobId} = req.params
-    const { company, position } = req.body
-    if(!company || !position) {
-        throw new BadRequestError('Please Provide All Values')
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    throw new BadRequestError("Please Provide All Values");
+  }
 
-    }
+  const job = await Job.findOne({ _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
 
-    const job = await Job.findOne({_id: jobId})
-    if(!job) {
-        throw new NotFoundError(`No job with id ${jobId}`)
-    }
+  // CHECK PERMISSION
 
-    // CHECK PERMISSION
+  checkPermission(req.user, job.createdBy);
 
-    const updatedJob = await Job.findOneAndUpdate({_id: jobId}, req.body, {
-        new: true,
-        runValidators: true,
-    })
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(StatusCodes.OK).json({ updatedJob })
-}
+  res.status(StatusCodes.OK).json({ updatedJob });
+};
 const deleteJob = async (req, res) => {
-    res.send('delete job')
-}
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new CustomError.NotFoundError(`No job with id : ${jobId}`);
+  }
+
+  checkPermission(req.user, job.createdBy);
+
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: "Success! Job removed" });
+};
 
 const showStats = async (req, res) => {
-    res.send('show stats')
-}
+  res.send("show stats");
+};
 
-export { createJob, deleteJob, getAllJobs, updateJob, showStats }
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
